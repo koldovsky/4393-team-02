4fb571f09e1a1a5820171908c9ad5645172b67d8
Artur Peretiatko <115393554+artur22877889@users.noreply.github.com>
2025-09-23 19:00:37 +0000

    add js tasks-02

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,1 +1,46 @@
+//Базове завдання 1
+function stringToArray(string){
+ return string.split(" ")
+}
+//Базове завдання 2
+function DNAtoRNA(dna) {
+  return dna.replaceAll('T','U');
+}
+//Поглиблене завдання 1
+var min = function(list) {
+  let smallest = list[0];
+  for (let i = 1; i < list.length; i++) {
+    if (list[i] < smallest) {
+      smallest = list[i]; 
+    }
+  }
+  return smallest;
+}
 
+var max = function(list) {
+  let largest = list[0];
+  for (let i = 1; i < list.length; i++) {
+    if (list[i] > largest) {
+      largest = list[i]; 
+    }
+  }
+  return largest;
+}
+//поглиблене завдання 1
+function min(arr, toReturn) {
+  let minValue = arr[0];
+  let minIndex = 0;
+  
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < minValue) {
+      minValue = arr[i];
+      minIndex = i;
+    }
+  }
+
+  if (toReturn === "value") {
+    return minValue;
+  } else if (toReturn === "index") {
+    return minIndex;
+  }
+}
