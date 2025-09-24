c214205c1fa62cec0411fa9da6833d7590a683a4
Mr. Schrödinger <114470355+Feshchenko1@users.noreply.github.com>
2025-09-24 15:50:18 +0300

    Update tasks-02.js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -6,11 +6,7 @@ function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
 
 //https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
 function makeNegative(num) {
-  if (num > 0) {
-    return -num;
-  } else {
-    return num;
-  }
+  return num > 0 ? -num : num;
 }
 
 //https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
@@ -20,11 +16,7 @@ function move(position, roll) {
 
 //https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
 function greet(name, owner) {
-  if (name === owner) {
-    return "Hello boss";
-  } else {
-    return "Hello guest";
-  }
+  return `Hello ${name === owner ? "boss" : "guest"}`;
 }
 
 //https://www.codewars.com/kata/keep-hydrated-1/train/javascript
@@ -34,14 +26,7 @@ function litres(time) {
 
 //https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
 function lovefunc(flower1, flower2) {
-  if (
-    (flower1 % 2 === 0 && flower2 % 2 !== 0) ||
-    (flower1 % 2 !== 0 && flower2 % 2 === 0)
-  ) {
-    return true;
-  } else {
-    return false;
-  }
+  return (flower1 + flower2) % 2 ? true : false;
 }
 
 //Заняття2
