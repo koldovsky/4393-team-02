664c4bc8f80fca02ddd7d93d322d8604a5a3448e
Iryna-Osypenko <irka992@gmail.com>
2025-09-24 11:34:01 +0000

    Updated tasks-02 js

--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -1,65 +1,115 @@
+//Завдання з першого заняття по JS
+//https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript
+function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+  return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+}
+
+//https://www.codewars.com/kata/55685cd7ad70877c23000102/train/javascript
+function makeNegative(num) {
+  if (num > 0) {
+    return -num;
+  } else {
+    return num;
+  }
+}
+
+//https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript
+function move(position, roll) {
+  return position + roll * 2;
+}
+
+//https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript
+function greet(name, owner) {
+  if (name === owner) {
+    return "Hello boss";
+  } else {
+    return "Hello guest";
+  }
+}
+
+//https://www.codewars.com/kata/keep-hydrated-1/train/javascript
+function litres(time) {
+  return Math.floor(time * 0.5);
+}
+
+//https://www.codewars.com/kata/555086d53eac039a2a000083/train/javascript
+function lovefunc(flower1, flower2) {
+  if (
+    (flower1 % 2 === 0 && flower2 % 2 !== 0) ||
+    (flower1 % 2 !== 0 && flower2 % 2 === 0)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//Заняття2
 //Базове завдання 1
-function stringToArray(string){
- return string.split(" ")
+function stringToArray(string) {
+  return string.split(" ");
 }
 
 //Базове завдання 2
 function DNAtoRNA(dna) {
-  return dna.replaceAll('T','U');
+  return dna.replaceAll("T", "U");
 }
 
 function DNAtoRNA(dna) {
-    let rna = dna.split("").map((item) => {
-        if(item.valueOf() === "t") {
-            item = "u";
+  let rna = dna
+    .split("")
+    .map((item) => {
+      if (item.valueOf() === "t") {
+        item = "u";
 
-            return item;
-        } else if (item.valueOf() === "T") {
-            item = "U";
+        return item;
+      } else if (item.valueOf() === "T") {
+        item = "U";
 
-            return item;
-        } else {
-            return item;
-        }
-    }).join("");
+        return item;
+      } else {
+        return item;
+      }
+    })
+    .join("");
 
-    return rna;
+  return rna;
 }
 
 //Поглиблене завдання 1
-var min = function(list) {
+var min = function (list) {
   let smallest = list[0];
   for (let i = 1; i < list.length; i++) {
     if (list[i] < smallest) {
-      smallest = list[i]; 
+      smallest = list[i];
     }
   }
   return smallest;
-}
+};
 
-var max = function(list) {
+var max = function (list) {
   let largest = list[0];
   for (let i = 1; i < list.length; i++) {
     if (list[i] > largest) {
-      largest = list[i]; 
+      largest = list[i];
     }
   }
   return largest;
-}
+};
 
-var min = function(list){
-    return list.sort((a, b) => a - b)[0];
-}
+var min = function (list) {
+  return list.sort((a, b) => a - b)[0];
+};
 
-var max = function(list){
-    return list.sort((a, b) => b - a)[0]
-}
+var max = function (list) {
+  return list.sort((a, b) => b - a)[0];
+};
 
 //поглиблене завдання 2
 function min(arr, toReturn) {
   let minValue = arr[0];
   let minIndex = 0;
-  
+
   for (let i = 1; i < arr.length; i++) {
     if (arr[i] < minValue) {
       minValue = arr[i];
@@ -74,12 +124,12 @@ function min(arr, toReturn) {
   }
 }
 
-function min(arr, toReturn) { 
-    let minNumber = Math.min(...arr);
+function min(arr, toReturn) {
+  let minNumber = Math.min(...arr);
 
-    if(toReturn === "value") {
-        return minNumber;
-    } else if (toReturn === "index") {
-        return arr.indexOf(minNumber);
-    }
+  if (toReturn === "value") {
+    return minNumber;
+  } else if (toReturn === "index") {
+    return arr.indexOf(minNumber);
+  }
 }
