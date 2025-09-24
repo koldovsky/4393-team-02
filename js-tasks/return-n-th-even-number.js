2d9c6123a1bb3ffbe72db7b0ae2e5390319b7737
Maksym Yankovych <yankovych1604@gmail.com>
2025-09-24 11:25:00 +0000

    Add alternative solutions for tasks

--- js-tasks/return-n-th-even-number.js
+++ js-tasks/return-n-th-even-number.js
@@ -1,3 +1,13 @@
 function nthEven(n){
   return (n - 1) * 2;
 }
+
+function nthEven(n){
+    let even = 0;
+
+    for(let i = 1; i < n; i++) {
+        even +=2;
+    }
+
+    return even;
+}
--- js-tasks/tasks-02.js
+++ js-tasks/tasks-02.js
@@ -2,10 +2,30 @@
 function stringToArray(string){
  return string.split(" ")
 }
+
 //Базове завдання 2
 function DNAtoRNA(dna) {
   return dna.replaceAll('T','U');
 }
+
+function DNAtoRNA(dna) {
+    let rna = dna.split("").map((item) => {
+        if(item.valueOf() === "t") {
+            item = "u";
+
+            return item;
+        } else if (item.valueOf() === "T") {
+            item = "U";
+
+            return item;
+        } else {
+            return item;
+        }
+    }).join("");
+
+    return rna;
+}
+
 //Поглиблене завдання 1
 var min = function(list) {
   let smallest = list[0];
@@ -26,7 +46,16 @@ var max = function(list) {
   }
   return largest;
 }
-//поглиблене завдання 1
+
+var min = function(list){
+    return list.sort((a, b) => a - b)[0];
+}
+
+var max = function(list){
+    return list.sort((a, b) => b - a)[0]
+}
+
+//поглиблене завдання 2
 function min(arr, toReturn) {
   let minValue = arr[0];
   let minIndex = 0;
@@ -43,4 +72,14 @@ function min(arr, toReturn) {
   } else if (toReturn === "index") {
     return minIndex;
   }
+}
+
+function min(arr, toReturn) { 
+    let minNumber = Math.min(...arr);
+
+    if(toReturn === "value") {
+        return minNumber;
+    } else if (toReturn === "index") {
+        return arr.indexOf(minNumber);
+    }
 }
